1d1c86682d8f94c7fdca3d341ffce5beb7732d7f
ongchangcoj <ongchangcoj@B101PROJ>
2024-04-29 09:47:50 +1200

    Add project files.

---
+++ CalcLibrary/CircleArea/Area_circ.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLibrary.CircleArea
+{
+    public class Area_circ
+    {
+        
+        public int AreaCalc()
+        {
+            int width;
+            int height;
+            int area;
+
+            //input
+            Console.Write("Input width: ");
+            width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input Height: ");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            area = width * height;
+            return area;
+
+        }
+    }
+}
---
+++ Week7DemoClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using CalcLibrary.CircleArea;
+
+namespace Week7DemoClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            int area1;
+
+            // made class Area_circ() dynamic so object is necessary
+            Area_circ areaCalc = new Area_circ();
+
+            // access class Area_circ and method AreaCalc inside library folder CircleArea
+            area1 = areaCalc.AreaCalc();
+            
+            
+
+            //int area= AreaCalc();
+
+            Console.Write($"The area is '{area1}'");
+        }
+
+        
+    }
+}
